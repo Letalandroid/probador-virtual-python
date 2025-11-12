e1026f41e584980e386183c28df2263cc6c6094a
Letalandroid <carlossoncra@gmail.com>
2025-11-12 12:44:41 -0500

    Actualizar la configuración de CORS para permitir todos los orígenes en lugar de los especificados.

--- src/api.py
+++ src/api.py
@@ -38,7 +38,7 @@ app = FastAPI(
 cors_origins = os.environ.get("CORS_ORIGINS", "http://localhost:5173,http://localhost:3000,http://localhost:8080,http://127.0.0.1:5173,http://127.0.0.1:8080,https://probador-virtual-ecommerce.vercel.app").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
